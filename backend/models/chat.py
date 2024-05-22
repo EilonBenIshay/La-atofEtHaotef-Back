e93e73ba3a492fc8381242e5afff3630d52c4ebf
Omerb90 <57324745+Omerb90@users.noreply.github.com>
2024-05-22 19:40:38 +0300

    Delete backend directory

--- backend/models/chat.py
+++
@@ -1,4 +0,0 @@
-class message:
-    def __init__(self, users, messageHistory):
-            self.users = users
-            self.messageHistory = messageHistory
--- backend/models/message.py
+++
@@ -1,5 +0,0 @@
-class message:
-    def __init__(self, text, sender, dateTime):
-        self.text = text
-        self.sender = sender
-        self.datetime = dateTime
