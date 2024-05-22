1a1b679d8a6c63d98f816f77a212550f1a9aecb7
Omerb90 <57324745+Omerb90@users.noreply.github.com>
2024-05-23 00:58:09 +0300

    token

--- facebook_scraper.py
+++ facebook_scraper.py
@@ -3,7 +3,7 @@ import requests
 def getPosts():
     posts = []
     facebook_request = requests.get(
- "https://graph.facebook.com/v20.0/me?fields=id%2Cname%2Cposts%7Bmessage%2Cfull_picture%7D&origin_graph_explorer=1&transport=cors&access_token=EAAGjIfPPdYsBO6YqZBMUGWz7QZBKH98VplDedASodeQ32IOnmaHh2ivOAsOZAZClZC75eNNfe4JTTfmevjZBoiIJ7eP12baY48TZBmAF5rV6W2XrBP97dZBCapJ5iUveznD50rqzomzMIpHX7Tn2HYr7ekR8IbmveoTE30ZBeIy9s4bBC2EZCSZBZCZCLoWZCfBA6Ih8baBnLH7ZCjgL6ZBFqO9xFXsQ9B6kiZAl9nPWHtdZAIq2ADJHMgZBrDkxZCdB"
+ "https://graph.facebook.com/v20.0/me?fields=id%2Cname%2Cposts%7Bmessage%2Cfull_picture%7D&origin_graph_explorer=1&transport=cors&access_token=EAAGjIfPPdYsBOzrWAi4JX74kd801Gh2ZBnYOpRMautU7WfMQuyG0N6hLvSkB70lAAmpnQFTuGBkTvjSOviTmrddJw4fPDsBCyZBBtNTcGx7BpsDJdhz8BPsOF3t7cn2ZCE8cUWHbeuvFFv2488GOojujHq3QQZBiKmHbh6APpVxlucclneNCrcHPdZAsZB5QAC7v5Pc644ZC9ve8vUpBejR3fdiI0WjhcL1DKahPako4UEBZCZBNrZBDYsRNypZBJ1vGwZDZD"
 )
     
     name = facebook_request.json()['name']
