1c30310e119abbe636ba9d712beda6d65b9c493e
Ori <Ori@example.com>
2024-05-23 05:03:15 +0300

    filter

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from instagram_scraper import get_all_posts
 from facebook_scraper import getPosts
 from flask_cors import CORS, cross_origin
+import psycopg2 
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,6 +12,30 @@ app.config["CORS_HEADERS"] = "Content-Type"
 @cross_origin()
 def hello():
     return 'Hello, World!'
+
+@app.route('/add_post', methods=['POST']) 
+def addPost(): 
+    conn = psycopg2.connect(database="sampledb", 
+                            user="userW2X", 
+                            password="3wDARjwQDHFToSw5", 
+                            host="10.130.2.60", port="5432") 
+  
+    cur = conn.cursor() 
+  
+    # Get the data from the form 
+    image = request.form['imageURL'] 
+    description = request.form['description'] 
+    userName = request.form['userName']
+    mode = request.form['mode'] 
+  
+    # Insert the data into the table 
+    cur.execute( 
+        '''INSERT INTO items \ 
+        (imageurl, username, description, mode) VALUES (%s, %s, %s, %s)''', 
+        (image, userName, description, mode)) 
+  
+    # commit the changes 
+    conn.commit() 
 
 @app.route('/Instagram')
 @cross_origin()
@@ -22,10 +47,12 @@ def getInstagramPage():
 
     return get_all_posts(USERNAMES)
 
-@app.route('/Facebook')
+@app.route('/Facebook', methods=['GET'])
 @cross_origin()
 def getFacebookPage():
-    return getPosts()
+    mode = int(request.args.get('mode', 0))
+    posts = getPosts()
+    return [p.to_json() for p in posts if p.mode == mode]
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9090)
--- facebook_scraper.py
+++ facebook_scraper.py
@@ -1,9 +1,10 @@
 import requests
+from post import Post
 
 def getPosts():
     posts = []
     facebook_request = requests.get(
- "https://graph.facebook.com/v20.0/me?fields=id%2Cname%2Cposts%7Bmessage%2Cfull_picture%7D&origin_graph_explorer=1&transport=cors&access_token=EAAGjIfPPdYsBO7hUSJqZAvefwIcBFejAIs59cYaRak7ENyPSbuDPlR09H0XmCfhYJM8VJwhfoKhgahWxJYefVsA6blBq12hop1LGEoSUFhcxe6ZAMSihdUlQ5aOPFDuckwPIdY0vXtqPHI0xhaPnZCRCFH5LUehikANyBg2GE6KX5mjl6x5EFLyg4qxOTZCVbcmSuwUm6ISiJTLxJMpu4CPj9oOz8nqJFH3TZAxZBXIoUvt6AydYKocU8BTM8P5AZDZD"
+ "https://graph.facebook.com/v20.0/me?fields=id%2Cname%2Cposts%7Bmessage%2Cfull_picture%7D&origin_graph_explorer=1&transport=cors&access_token=EAAGjIfPPdYsBO5ZBoFEh8Y9OBK8EM17zlGj84WMiHpJlZBGFmscGLUSad85GofjMH0HyAsJBSNBPiI8OR5DNmdAE6vQeubL9AZAFWPitThMHqBUUEY7daMH0uxvZCyID6jpvb3RENXFwky4OJmTU1hQedq0ZBujy98bc5Q6uHyrE0vmpMceqLJ1eS"
 )
     
     name = facebook_request.json()['name']
@@ -12,13 +13,20 @@ def getPosts():
     for data in fbposts[:-1]:
         picture = ""
         message = ""
+        createdDate = ""
         if 'full_picture' in data.keys():
             picture = data['full_picture']
         if 'message' in data.keys():
             message = data['message']
+        if 'created_date' in data.keys():
+            createdDate = data['created_date']
         
-        posts.append({"userName" : name,
-                      "imageURL" : picture,
-                      "description" : message})
+        if "מחפש " in message:
+            mode = 1
+        elif "למסירה " in message:
+            mode = 0
+        else:
+            mode = 2
         
+        posts.append(Post(name, createdDate, picture, message, mode, "Facebook"))
     return posts
